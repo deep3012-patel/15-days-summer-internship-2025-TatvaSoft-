6850983568a258e7444ae86b51c1b06e0f73dde0
deep3012-patel <deeppatel30122003@gmail.com>
2025-05-29 14:57:11 +0530

    Add files via upload

---
+++ day4/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookManagementSystem.Models;
+
+namespace BookManagementSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
